71c9c8b27b2fbde712d6537c09d10e794055a448
Murilo Missura <murilo@Mac-mini-de-Murilo.local>
2024-02-05 21:32:55 -0300

    feat: insert links

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,40 @@ export const App = () => {
         </h2>
         <div className="grid grid-cols-12 gap-7 2xl:my-20 xl:my-20 lg:my-20 my-12">
           <div className="2xl:w-52 xl:w-52 lg:w-52 w-full h-52 2xl:col-span-3 xl:col-span-3 lg:col-span-6 col-span-12 bg-white flex justify-center items-center">
-            Sua imobiliária aqui
+            <a href='https://www.rlimob.com.br/'
+            target="_blank"
+            className="bg-white text-black py-3 px-10 text-center mt-12 flex justify-center items-center gap-3 rounded-xl mb-12"
+            >
+              Imobiliaria 1
+            </a>
+            {/* Sua imobiliária aqui */}
           </div>
           <div className="2xl:w-52 xl:w-52 lg:w-52 w-full h-52 2xl:col-span-3 xl:col-span-3 lg:col-span-6 col-span-12 bg-white flex justify-center items-center">
-            Sua imobiliária aqui
+            <a href='https://www.capitalimoveis.com.br/'
+            target="_blank"
+            className="bg-white text-black py-3 px-10 text-center mt-12 flex justify-center items-center gap-3 rounded-xl mb-12"
+            >
+              Imobiliaria 2
+            </a>
+            {/* Sua imobiliária aqui */}
           </div>
           <div className="2xl:w-52 xl:w-52 lg:w-52 w-full h-52 2xl:col-span-3 xl:col-span-3 lg:col-span-6 col-span-12 bg-white flex justify-center items-center">
-            Sua imobiliária aqui
+            <a href='https://www.imobiliariasmart.net/'
+            target="_blank"
+            className="bg-white text-black py-3 px-10 text-center mt-12 flex justify-center items-center gap-3 rounded-xl mb-12"
+            >
+              Imobiliaria 3
+            </a>
+            {/* Sua imobiliária aqui */}
           </div>
           <div className="2xl:w-52 xl:w-52 lg:w-52 w-full h-52 2xl:col-span-3 xl:col-span-3 lg:col-span-6 col-span-12 bg-white flex justify-center items-center">
-            Sua imobiliária aqui
+            <a href='https://www.gabrielalessander.com.br/'
+            target="_blank"
+            className="bg-white text-black py-3 px-10 text-center mt-12 flex justify-center items-center gap-3 rounded-xl mb-12"
+            >
+              Imobiliaria 4
+            </a>
+            {/* Sua imobiliária aqui */}
           </div>
         </div>
         <h2 className="text-2xl text-center 2xl:text-4xl xl:text-4xl lg:text-2xl mb-8 text-yellow-300 px-12">
